f13dd0fc305c8264c2a44930a3f5349332bae832
dariomontani <dariomontani@gmail.com>
2021-11-29 15:59:58 +0100

    chiedo e scompongo la parola dell'utente

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -3,3 +3,15 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 // (prima senza funzione e poi con funzione)
 
+
+// chiedo all'utente di inserire una parola
+let parolaUtente = prompt('Inserisci una parola');
+
+// scompongo la parola dell'utente
+let parolaScomposta = parolaUtente.split('');
+console.log(parolaScomposta);
+
+
+
+
+
