fbafb07d1ef3840e7f9cbd341fd4a1f386f0dd6a
dariomontani <dariomontani@gmail.com>
2021-11-29 16:16:20 +0100

    scompongo e inverto la parola

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -3,15 +3,22 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 // (prima senza funzione e poi con funzione)
 
+// creo due array vuoti dove andrò ad inserire la parola scomposta e la parola al contrario per verificare se sono uguali
+const arrayParola = [];
+const arrayParolaContrario = [];
 
-// chiedo all'utente di inserire una parola
+// chiedo all'utente di inserire una parola 
 let parolaUtente = prompt('Inserisci una parola');
 
-// scompongo la parola dell'utente
+// scompongo la parola dell'utente e la inserisco nell'arrayParola
 let parolaScomposta = parolaUtente.split('');
-console.log(parolaScomposta);
+arrayParola.push = parolaScomposta;
+console.log('parola scomposta',arrayParola);
 
-
+// inverto l'ordine delle lettere della parola dell'utente e la inserisco nell'array
+let parolaContrario = parolaScomposta.reverse();
+arrayParolaContrario.push = parolaContrario;
+console.log('parola Contrario', arrayParolaContrario);
 
 
 
