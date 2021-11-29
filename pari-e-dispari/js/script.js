cc4abb63e9ec525d4ad6074471a3d776327ad83c
dariomontani <dariomontani@gmail.com>
2021-11-29 17:36:46 +0100

    verifico se l'utente ha vinto o ha perso

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -7,12 +7,29 @@
 // (corrispondenza tra scelta pari e dispari e somma pari o dispari)
 // In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
 
+// L’utente sceglie pari o dispari
+const sceltaPariDispari = prompt('scegli pari o dispari');
+console.log('pari o dispari', sceltaPariDispari);
 
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const numeroUtente = parseInt(prompt('inserisci un munero'));
-console.log(numeroUtente);
+// L’utente inserisce un numero da 1 a 5.
+const numeroUtente = parseInt(prompt('inserisci un munero da 1 a 5'));
+console.log('Utente', numeroUtente);
 
 // Generiamo un numero random(sempre da 1 a 5) per il computer
 const numeroComputer = Math.floor(Math.random() * 10) + 1;
-console.log(numeroComputer);
+console.log('computer', numeroComputer);
+
+// Sommiamo i due numeri
+let somma = numeroUtente + numeroComputer;
+console.log('somma', somma);
+
+// verifico se l'utente ha vinto o gha perso
+if (somma % 2 == 0 && sceltaPariDispari == 'pari') {
+    console.log('HAI VINTO CON PARI');
+} else if (somma % 2 == 1 && sceltaPariDispari == 'dispari') {
+    console.log('HAI VINTO CON DISPARI');
+} else {
+    console.log('HAI PERSO');
+}
+
 
